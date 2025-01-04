9136731b570d38a10c1eab6597433aed331218ba
NokibReja <nokibreja414@gmail.com>
2025-01-04 23:45:34 +0600

    second

---
+++ grade-calculator.js
@@ -0,0 +1,16 @@
+const result = 101;
+
+
+if (result>=90 && result<=100) {
+    console.log('you got A')
+} else if(result>=80 && result<=89) {
+    console.log('You got B')
+}else if (result>=70 && result<=79) {
+    console.log('You got C')
+} else if (result>=60 && result<=69) {
+    console.log('You got D')
+}else if(result>=0 && result<=59){
+    console.log('You got F')
+}else{
+    console.log('Wrong Result Input')
+}
